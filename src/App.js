d50e46318bdc099d2c3411964d20e64957bb2305
venkata122528 <venkata122528@gmail.com>
2024-04-02 12:59:02 +0000

    Debugging Nxt Trendz Protected Route

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-named-as-default-member */
+import {Switch, Route} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+// eslint-disable-next-line import/no-named-as-default
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
